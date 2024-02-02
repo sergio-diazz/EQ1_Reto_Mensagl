1190832fb3287c4b11ef28fe0e4f45c9b2221f28
sergio-diazz <155975588+sergio-diazz@users.noreply.github.com>
2024-02-02 10:35:45 +0100

    Delete Scripts-DatosUsuario-EC2 directory

--- Scripts-DatosUsuario-EC2/coturn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Script creado por Sergio Diaz Lastra
-#25/01/2024
-#Instalación coturn
-sudo apt-get update
-sudo apt-get install coturn -y
--- Scripts-DatosUsuario-EC2/nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Script creado por Sergio Diaz Lastra
-#25/01/2024
-#Instalación nginx y certbot para certificados
-sudo apt-get update
-sudo apt-get install nginx -y
-sudo apt-get install python3-certbot-nginx -y
