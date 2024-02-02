0972c115015e6a682e78d53f72c294351884e986
sergio-diazz <155975588+sergio-diazz@users.noreply.github.com>
2024-02-02 10:39:30 +0100

    Actualizado

--- Scripts instalacion/matrixturn.sh
+++ Scripts instalacion/matrixturn.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Script creado por Sergio Diaz Lastra
+#25/01/2024
+#Cambiar el archivo de configuración de turn en el servidor matrix
 cd /etc/matrix-synapse/conf.d
 echo "turn_uris: [ "turns:equipo1coturn.duckdns.org?transport=udp", "turns:equipo1coturn.duckdns.org?transport=tcp" ] #cambiar dominio
 turn_shared_secret: "0OzqH2XmIV6BiWvultmhG679vVHqtdJvserJbZrJcdlTVFpwxGXzaxzzW1OnKu59" #poner la clave que te dieron en el turn
