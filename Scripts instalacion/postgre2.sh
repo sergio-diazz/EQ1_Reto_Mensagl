a351f761dbd3b5e7ad27cca1a8a11e613d957ec2
sergio-diazz <155975588+sergio-diazz@users.noreply.github.com>
2024-02-02 10:45:40 +0100

    Actualizado

--- Scripts instalacion/postgre2.sh
+++ Scripts instalacion/postgre2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Script creado por Sergio Diaz Lastra
+#25/01/2024
+#Creación de usuario y base datos
 createuser --pwprompt synapse
 createdb --encoding=UTF8 --locale=C --template=template0 --owner=synapse synapsedb
 exit
