3de366d92baf0bcc5dfe01c547addafa85f674dc
sergio-diazz <155975588+sergio-diazz@users.noreply.github.com>
2024-02-02 10:35:55 +0100

    Delete Scripts-Instalacion directory

--- Scripts-Instalacion/matrixturn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /etc/matrix-synapse/conf.d
-echo "turn_uris: [ "turns:equipo1coturn.duckdns.org?transport=udp", "turns:equipo1coturn.duckdns.org?transport=tcp" ] #cambiar dominio
-turn_shared_secret: "0OzqH2XmIV6BiWvultmhG679vVHqtdJvserJbZrJcdlTVFpwxGXzaxzzW1OnKu59" #poner la clave que te dieron en el turn
-turn_user_lifetime: 86400000
-turn_allow_guests: True" > turn.yaml
--- Scripts-Instalacion/postgre2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-createuser --pwprompt synapse
-createdb --encoding=UTF8 --locale=C --template=template0 --owner=synapse synapsedb
-exit
